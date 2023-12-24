b12d217b0585851c6e4d69db4eb826111c48847f
Mohammad Zakir Hossain <engineer.zakir.hossain@gmail.com>
2023-12-24 22:33:18 +0200

    annonymous-function

--- arrow-function.js
+++ arrow-function.js
@@ -6,10 +6,16 @@ function add(num1,num2){
 const addNumbers = add(12,13);
 console.log("Addition output: " + addNumbers);
 
-// Arrow function for multiplication
+// Annonymous function for multiplication
 const multiply = function(num1, num2){
     return num1 * num2;
 }
 
 const multiplyResult = multiply(3,2);
 console.log('Multiplication output:' + multiplyResult);
+
+// Arrow function
+
+const division = (num1,num2) => num1/num2;
+const divisionResult = division(14,2);
+console.log("Division output: " + divisionResult);
