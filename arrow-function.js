f0f408fa14168009ab00d1000afb941c9dc336c9
Mohammad Zakir Hossain <engineer.zakir.hossain@gmail.com>
2023-12-24 22:27:56 +0200

    ES06

---
+++ arrow-function.js
@@ -0,0 +1,15 @@
+// Normal function for addition
+function add(num1,num2){
+    return num1+num2;
+}
+
+const addNumbers = add(12,13);
+console.log("Addition output: " + addNumbers);
+
+// Arrow function for multiplication
+const multiply = function(num1, num2){
+    return num1 * num2;
+}
+
+const multiplyResult = multiply(3,2);
+console.log('Multiplication output:' + multiplyResult);
