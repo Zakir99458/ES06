332eb08ce051ccefad16f096dd9535effefd9fd9
Mohammad Zakir Hossain <engineer.zakir.hossain@gmail.com>
2023-12-25 11:36:14 +0200

    mod-class

--- class.js
+++ class.js
@@ -1,11 +1,16 @@
 class Support{
-    // name;
+    name;
     designation = 'Web development';
+    address;
+    constructor(p_name, p_addr){
+        this.name = p_name;
+        this.address = p_addr;
+    }
     startSession(){
         console.log(`Session started.....${this.designation}`);
     }
 }
 
-const aamir = new Support();
-console.log(aamir.startSession());
+const aamir = new Support('Aamir Khan',"Finland");
+console.log(aamir);
 
