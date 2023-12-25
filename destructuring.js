d6413f1dd5049ff8226f7a1b66733372d808412f
Mohammad Zakir Hossain <engineer.zakir.hossain@gmail.com>
2023-12-25 09:59:52 +0200

    obj-destructuring

---
+++ destructuring.js
@@ -0,0 +1,5 @@
+const fish = {id: 98, name: "Hilsha", phone: '01684030291', address: "vantaa"};
+
+const {phone, id, address} = fish;
+
+console.log(phone);
--- spread.js
+++ spread.js
@@ -2,4 +2,10 @@ const numbers = [4,5,6,7,8];
 console.log(...numbers);
 console.log(numbers);
 
+// find the maximum number from an array value
+const arrayNumbers = [9,20,2,9,99,87];
+const maxValue = Math.max(...arrayNumbers);
+console.log(maxValue);
+
+
 
