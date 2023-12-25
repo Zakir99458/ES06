9f3b54ee113ac1ed0fc9ccd214cba19811922483
Mohammad Zakir Hossain <engineer.zakir.hossain@gmail.com>
2023-12-25 10:15:10 +0200

    ecma-object

--- destructuring.js
+++ destructuring.js
@@ -3,3 +3,14 @@ const fish = {id: 98, name: "Hilsha", phone: '01684030291', address: "vantaa"};
 const {phone, id, address} = fish;
 
 console.log(phone);
+
+// array of objects
+const company = {
+    name: 'GP',
+    dept: 'IT',
+    web:{work: 'web development', framework:'cakePHP'}
+}
+
+const {framework} = company.web;
+console.log(framework);
+console.log(company.name);
